71d0c815318ea4765bb1f103d81a5de4b8e18393
Tyler Howell <tyler.howell1202@gmail.com>
2024-02-27 18:53:15 -0600

    Initial refactor commit, made seam trapping Normal item

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,6 +8,11 @@ class GildedRose
   end
 
   def tick
+
+    if name == 'Normal Item'
+      return
+    end
+    
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
         if @name != "Sulfuras, Hand of Ragnaros"
